aa03216ae3928e3d8ea8145a3dcad41cf4247223
麻智敏 <ZhiMinMa@sf-express.com>
2020-09-03 20:16:25 +0800

    登录

--- admin-web/src/main/java/com/zh/cloud/admin/controller/upms/UserController.java
+++ admin-web/src/main/java/com/zh/cloud/admin/controller/upms/UserController.java
@@ -1,48 +1,38 @@
-package com.zh.cloud.admin.controller;
+package com.zh.cloud.admin.controller.upms;
 
 import com.ch.result.PageResult;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.zh.cloud.admin.controller.LoginController;
 import com.zh.cloud.admin.model.BaseModel;
 import com.zh.cloud.admin.model.User;
 import com.zh.cloud.admin.service.UserService;
 import io.ebean.PagedList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 用户管理控制层
  *
- * @author zhimin.ma 2019-07-13 下午05:12:16
+ * @author zhimin.ma 2020-07-13 下午05:12:16
  * @version 1.0.0
  */
 @RestController
 @RequestMapping("/api/{env}/user")
 public class UserController {
 
-    public static final LoadingCache<String, User> loginUsers = Caffeine.newBuilder()
-            .maximumSize(10_000)
-            .expireAfterAccess(30, TimeUnit.MINUTES)
-            .build(key -> null); // 用户登录信息缓存
-
     @Autowired
     private UserService userService;
 
     /**
-     * 用户登录
+     * 用户分页查询
      *
-     * @param record 账号密码
-     * @param env    环境变量
+     * @param record   用户信息
+     * @param env      环境变量
+     * @param pageNum  页码
+     * @param pageSize 页大小
      * @return token
      */
-//    @PostMapping(value = "/{page}/{size}")
     @GetMapping(value = {"/{num:[0-9]+}/{size:[0-9]+}"})
     public PageResult<User> page(User record,
                                  @PathVariable(value = "num") int pageNum,
@@ -61,7 +51,7 @@ public class UserController {
      */
     @GetMapping(value = "/{id}")
     public BaseModel<User> info(@RequestParam String token, @PathVariable String env, @PathVariable Long id) {
-        User user = loginUsers.getIfPresent(token);
+        User user = LoginController.loginUsers.getIfPresent(token);
         if (user != null) {
             return BaseModel.getInstance(user);
         } else {
@@ -85,7 +75,7 @@ public class UserController {
                                     HttpServletRequest httpServletRequest) {
         userService.update(user);
         String token = (String) httpServletRequest.getAttribute("token");
-        loginUsers.put(token, user);
+        LoginController.loginUsers.put(token, user);
         return BaseModel.getInstance("success");
     }
 
