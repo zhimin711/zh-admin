3b97a3d1354b1d5da55a5f448e958a0c8c68c095
仙_梦如尘 <zhimin711@sina.com>
2020-09-19 14:03:30 +0800

    组织职位分配添加

--- admin-ui2/src/view/upms/user/index.vue
+++ admin-ui2/src/view/upms/user/index.vue
@@ -1,101 +1,120 @@
 <template>
   <div>
-    <Card>
-      <Form ref="formInline" :model="pq.params" inline :label-width="80">
-        <FormItem prop="username" label="用户名">
-          <Input type="text" v-model="pq.params.username" placeholder="用户名">
-          </Input>
-        </FormItem>
-        <FormItem prop="name" label="用户名称">
-          <Input type="text" v-model="pq.params.name" placeholder="名称">
-          </Input>
-        </FormItem>
-      </Form>
-      <Button style="margin: 5px 3px;" type="primary" icon="md-search" @click="handleSearch">{{ $t('searchText') }}</Button>
-      <Button style="margin: 5px 3px;" icon="md-refresh" @click="pq.params = {}">{{ $t('resetText') }}</Button>
-      <Button v-permission="['upmsUserAdd']" style="margin: 5px 3px;" type="primary" icon="md-add" @click="handleAdd">{{ $t('addText') }}</Button>
-      <Button v-permission="['upmsUserRole']" style="margin: 5px 3px;" type="success" icon="md-people" @click="handleRoles" :disabled="selectRows.length!==1">分配角色</Button>
-      <Table ref="tables" :data="tableData" :columns="columns" @on-select="handleSelect" @on-select-cancel="handleSelect">
-        <template slot-scope="{ row, index }" slot="action">
-          <ButtonGroup size="small">
-            <Button v-permission="['upmsUserEdit']" icon="ios-create-outline" @click="handleEdit(row, index)">编辑</Button>
-            <Button v-permission="['upmsUserDel']"  icon="ios-remove" @click="handleDelete(row,index)">删除</Button>
-          </ButtonGroup>
-        </template>
-      </Table>
-      <Page :total="pq.total" :current.sync="pq.page" @on-change="handlePage" @on-page-size-change="handleSize" show-sizer show-elevator show-total />
+    <Row>
+      <Col span="6" style="padding-right: 7px">
 
-      <Modal v-model="recordModal" @on-cancel="cancelRecord">
-        <p slot="header">
-          <span>用户</span>
-          <span v-if="recordModalType === 'add'">新增</span>
-          <span v-if="recordModalType === 'edit'">修改</span>
-        </p>
-        <Form ref="recordForm" :model="record" :rules="recordRules" :label-width="80">
-          <FormItem label="账号" prop="userId" required>
-            <i-input type="text" v-model="record.userId" :disabled="disabledProps.code"></i-input>
-          </FormItem>
-          <FormItem label="用户名" prop="username" required>
-            <i-input type="text" v-model="record.username" :disabled="disabledProps.code"></i-input>
-          </FormItem>
-          <FormItem label="真实姓名" prop="realName" required>
-            <i-input type="text" v-model="record.realName"></i-input>
-          </FormItem>
-          <FormItem label="性别">
-            <RadioGroup v-model="recordSex">
-              <Radio label="1">男</Radio>
-              <Radio label="0">女</Radio>
-            </RadioGroup>
-          </FormItem>
-          <FormItem label="出生日期" prop="birth">
-            <DatePicker type="date" v-model="record.birth" placeholder="Select date" style="width: 200px" @on-change="changeDateBirth"></DatePicker>
-          </FormItem>
-          <FormItem label="手机号码" prop="mobile">
-            <i-input type="text" v-model="record.mobile"></i-input>
-          </FormItem>
-          <FormItem label="电子邮箱" prop="email">
-            <i-input type="text" v-model="record.email"></i-input>
-          </FormItem>
-          <FormItem label="过期日期" prop="expired">
-            <DatePicker type="date" v-model="record.expired" placeholder="Select date" style="width: 200px"></DatePicker>
-          </FormItem>
-          <FormItem label="状态" prop="status">
-            <i-switch size="large" v-model="recordStatus">
-              <span slot="open">开启</span>
-              <span slot="close">关闭</span>
-            </i-switch>
-          </FormItem>
-        </Form>
-        <div slot="footer">
-          <Button type="primary" @click="handleSubmit('recordForm')" :loading="loading">提交</Button>
-          <Button @click="cancelRecord" style="margin-left: 8px" :disabled="loading">取消</Button>
-        </div>
-      </Modal>
-      <Modal v-model="roleModal" size="large">
-        <p slot="header">
-          请注意，您当前正在对 <span style="color:red; vertical-align: top;">{{record.username}}</span> 用户进行角色分配操作！
-        </p>
-        <!--:render-format="render3"-->
-        <Transfer
-          :data="roles"
-          :target-keys="valueRoles"
-          :list-style="listStyle"
-          :titles="['可分配角色','已分配角色']"
-          :operations="['移除','分配']"
-          filterable
-          @on-change="changeRole">
-          <div :style="{float: 'right', margin: '5px'}">
-            <Button size="small" @click="reloadRoles">{{ $t('refreshText') }}</Button>
-          </div>
-        </Transfer>
-        <div slot="footer">
-          <Button type="primary" @click="saveUserRoles" :loading="loading">提交</Button>
-          <Button @click="()=>{this.roleModal = false;this.valueRoles = []}" style="margin-left: 8px"
-                  :disabled="loading">取消
-          </Button>
-        </div>
-      </Modal>
-    </Card>
+        <Card>
+          <Tree :data="departmentTree" :render="renderDepartmentTree" @on-select-change="searchDepartment">
+          </Tree>
+        </Card>
+      </Col>
+      <Col span="18">
+        <Card>
+          <Form ref="formInline" :model="pq.params" inline :label-width="80">
+            <FormItem prop="username" label="用户名">
+              <Input type="text" v-model="pq.params.username" placeholder="用户名">
+              </Input>
+            </FormItem>
+            <FormItem prop="name" label="用户名称">
+              <Input type="text" v-model="pq.params.name" placeholder="名称">
+              </Input>
+            </FormItem>
+          </Form>
+          <Button style="margin: 5px 3px;" type="primary" icon="md-search" @click="handleSearch">{{ $t('searchText') }}</Button>
+          <Button style="margin: 5px 3px;" icon="md-refresh" @click="pq.params = {}">{{ $t('resetText') }}</Button>
+          <Button v-permission="['upmsUserAdd']" style="margin: 5px 3px;" type="primary" icon="md-add" @click="handleAdd">{{ $t('addText') }}</Button>
+          <Button v-permission="['upmsUserRole']" style="margin: 5px 3px;" type="success" icon="md-people" @click="handleRoles" :disabled="selectRows.length!==1">分配角色</Button>
+          <Table ref="tables" :data="tableData" :columns="columns" @on-select="handleSelect" @on-select-cancel="handleSelect">
+            <template slot-scope="{ row, index }" slot="action">
+              <ButtonGroup size="small">
+                <Button v-permission="['upmsUserEdit']" icon="ios-create-outline" @click="handleEdit(row, index)">编辑</Button>
+                <Button v-permission="['upmsUserDel']"  icon="ios-remove" @click="handleDelete(row,index)">删除</Button>
+              </ButtonGroup>
+            </template>
+          </Table>
+          <Page :total="pq.total" :current.sync="pq.page" @on-change="handlePage" @on-page-size-change="handleSize" show-sizer show-elevator show-total />
+
+          <Modal v-model="recordModal" @on-cancel="cancelRecord">
+            <p slot="header">
+              <span>用户</span>
+              <span v-if="recordModalType === 'add'">新增</span>
+              <span v-if="recordModalType === 'edit'">修改</span>
+            </p>
+            <Form ref="recordForm" :model="record" :rules="recordRules" :label-width="80">
+              <FormItem label="账号" prop="userId" required>
+                <i-input type="text" v-model="record.userId" :disabled="disabledProps.code"></i-input>
+              </FormItem>
+              <FormItem label="用户名" prop="username" required>
+                <i-input type="text" v-model="record.username" :disabled="disabledProps.code"></i-input>
+              </FormItem>
+              <FormItem label="真实姓名" prop="realName" required>
+                <i-input type="text" v-model="record.realName"></i-input>
+              </FormItem>
+              <FormItem label="性别">
+                <RadioGroup v-model="recordSex">
+                  <Radio label="1">男</Radio>
+                  <Radio label="0">女</Radio>
+                </RadioGroup>
+              </FormItem>
+              <FormItem label="出生日期" prop="birth">
+                <DatePicker type="date" v-model="record.birth" placeholder="Select date" style="width: 200px" @on-change="changeDateBirth"></DatePicker>
+              </FormItem>
+              <FormItem label="手机号码" prop="mobile">
+                <i-input type="text" v-model="record.mobile"></i-input>
+              </FormItem>
+              <FormItem label="电子邮箱" prop="email">
+                <i-input type="text" v-model="record.email"></i-input>
+              </FormItem>
+              <FormItem label="所属部门" prop="departmentId">
+                <Cascader :data="options.department" v-model="values.department" :render-format="parentFormat" :disabled="disabledProps.department" change-on-select @on-change="departmentChange"></Cascader>
+              </FormItem>
+              <FormItem label="部门职位" prop="positionId">
+                <Select v-model="record.positionId" style="width:200px" :label-in-value="labelInValue" @on-change="positionSelect">
+                  <Option v-for="item in options.position" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                </Select>
+              </FormItem>
+              <FormItem label="过期日期" prop="expired">
+                <DatePicker type="date" v-model="record.expired" placeholder="Select date" style="width: 200px"></DatePicker>
+              </FormItem>
+              <FormItem label="状态" prop="status">
+                <i-switch size="large" v-model="recordStatus">
+                  <span slot="open">开启</span>
+                  <span slot="close">关闭</span>
+                </i-switch>
+              </FormItem>
+            </Form>
+            <div slot="footer">
+              <Button type="primary" @click="handleSubmit('recordForm')" :loading="loading">提交</Button>
+              <Button @click="cancelRecord" style="margin-left: 8px" :disabled="loading">取消</Button>
+            </div>
+          </Modal>
+          <Modal v-model="roleModal" size="large">
+            <p slot="header">
+              请注意，您当前正在对 <span style="color:red; vertical-align: top;">{{record.username}}</span> 用户进行角色分配操作！
+            </p>
+            <!--:render-format="render3"-->
+            <Transfer
+              :data="roles"
+              :target-keys="valueRoles"
+              :list-style="listStyle"
+              :titles="['可分配角色','已分配角色']"
+              :operations="['移除','分配']"
+              filterable
+              @on-change="changeRole">
+              <div :style="{float: 'right', margin: '5px'}">
+                <Button size="small" @click="reloadRoles">{{ $t('refreshText') }}</Button>
+              </div>
+            </Transfer>
+            <div slot="footer">
+              <Button type="primary" @click="saveUserRoles" :loading="loading">提交</Button>
+              <Button @click="()=>{this.roleModal = false;this.valueRoles = []}" style="margin-left: 8px"
+                      :disabled="loading">取消
+              </Button>
+            </div>
+          </Modal>
+        </Card>
+      </Col>
+    </Row>
   </div>
 </template>
 
@@ -103,18 +122,20 @@
 import Tables from '_c/tables'
 import { getPageUser, getUserRoles, saveUserRoles, addUser, editUser } from '@/api/upms/user'
 import { getAllRole } from '@/api/upms/role'
+import { getTreeDepartment, getDepartmentPositions } from '@/api/upms/department'
 
 const defaultR = {
   username: '',
   password: ''
 }
 export default {
-  name: 'upmsUser',
+  name: 'upmsUser1',
   components: {
     Tables
   },
   data () {
     return {
+      labelInValue: true,
       roleModal: false,
       roles: [],
       valueRoles: [],
@@ -139,11 +160,12 @@ export default {
           width: 60,
           align: 'center'
         },
+        { title: '部门', key: 'departmentName' },
         { title: '用户账号', key: 'userId' },
         { title: '用户名', key: 'username' },
         { title: '用户昵称', key: 'realName' },
         { title: '邮箱', key: 'email' },
-        { title: '创建时间', key: 'createAt' },
+        // { title: '创建时间', key: 'createAt' },
         {
           title: '操作',
           slot: 'action',
@@ -159,12 +181,70 @@ export default {
         total: 0
       },
       selectRows: [],
-      tableData: []
+      tableData: [],
+      options: {
+        department: [],
+        position: []
+      },
+      values: {
+        department: [],
+        label: []
+      }
+    }
+  },
+  computed: {
+    departmentTree () {
+      return this.covertDepartment(this.options.department)
     }
   },
   methods: {
+    covertDepartment(rows, pid) {
+      let tree= []
+      rows.forEach(item => {
+        let dept = {title: item.label, sid: item.value}
+        if(pid) {
+          dept.sid = pid + ',' + item.value
+        }
+        if(item.children && item.children.length > 0){
+          dept.expand = true
+          dept.children = this.covertDepartment(item.children, dept.sid)
+        }
+        tree.push(dept)
+      })
+      return tree
+    },
+    renderDepartmentTree (h, { root, node, data }) {
+      return h('span', {
+        style: {
+          display: 'inline-block',
+          width: '100%'
+        },
+        on:{
+          click:()=>{
+            this.searchDepartment(data)
+          }
+        }
+      }, [
+        h('span', [
+          h('Icon', {
+            props: {
+              type: 'ios-paper-outline'
+            },
+            style: {
+              marginRight: '8px'
+            }
+          }),
+          h('span', data.title)
+        ])
+      ]);
+    },
+    searchDepartment(v){
+      this.pq.params.departmentId = v.sid
+      this.getPageUser()
+    },
     handleSearch () {
       this.pq.page = 1
+      this.pq.params.departmentId = undefined
       this.getPageUser()
     },
     handlePage (val) {
@@ -200,6 +280,11 @@ export default {
       } else {
         this.recordSex = '0'
       }
+      this.values.department = []
+      if (row.departmentId) {
+        this.values.department = row.departmentId.split(',').map(item => Number(item))
+        this.getDepartmentPositions(this.values.department)
+      }
     },
     async handleSubmit () {
       let resp
@@ -210,6 +295,12 @@ export default {
         this.record.sex = undefined
       } else {
         this.record.sex = this.recordSex === '1'
+      }
+      this.record.departmentId = ''
+      this.record.departmentName = ''
+      if (this.values.department.length > 0) {
+        this.record.departmentId = this.values.department.join(',')
+        this.record.departmentName = this.values.label.join('/')
       }
 
       this.loading = true
@@ -285,6 +376,33 @@ export default {
       this.getRoles()
       this.getUserRoles()
     },
+    parentFormat (label) {
+      this.values.label = label
+      return label.join('/')
+    },
+    getDepartments () {
+      getTreeDepartment(2).then(res => {
+        const { data } = res
+        this.options.department = data.rows
+      })
+    },
+    departmentChange (value) {
+      this.getDepartmentPositions(value)
+    },
+    getDepartmentPositions (ids) {
+      this.options.position = []
+      if (ids.length > 0) {
+        getDepartmentPositions(ids[ids.length - 1]).then(resp => {
+          const { data } = resp
+          if (data && data.success) {
+            this.options.position = data.rows
+          }
+        })
+      }
+    },
+    positionSelect (data) {
+      this.record.positionName = data.label
+    },
     getPageUser () {
       getPageUser(this.pq).then(res => {
         this.tableData = res.data.rows
@@ -295,6 +413,7 @@ export default {
   mounted () {
     this.getPageUser()
     this.getRoles()
+    this.getDepartments()
   }
 }
 </script>
